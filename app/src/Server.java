7db54b8abbec10295ea5db5ab602166ce3589783
lapin-anton <lapin_antoine@mail.ru>
2020-12-13 18:48:47 +0300

    added inner class Handler into class Server, added field, constructor, and empty method "run" into class Handler, added realization into method "main" by class Server

--- app/src/Server.java
+++ app/src/Server.java
@@ -1,5 +1,32 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер запущен.");
+            while (true) {
+                Socket s = serverSocket.accept();
+                Handler handler = new Handler(s);
+                handler.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 }

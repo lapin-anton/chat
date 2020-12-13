d878a811d0a018c5b79598177261e090e42e378b
lapin-anton <lapin_antoine@mail.ru>
2020-12-13 18:11:49 +0300

    added empty methods into ConsoleHelper class

--- app/src/ConsoleHelper.java
+++ app/src/ConsoleHelper.java
@@ -1,2 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        return null;
+    }
+
+    public static int readInt() {
+        return 0;
+    }
 }

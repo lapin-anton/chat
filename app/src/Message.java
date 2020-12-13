75b24c68733ec8882ff8386462b5d778179199bc
lapin-anton <lapin_antoine@mail.ru>
2020-12-13 18:04:40 +0300

    added realization into Message and MessageType classes

--- app/src/Message.java
+++ app/src/Message.java
@@ -1,2 +1,22 @@
 public class Message {
+    private final MessageType type;
+    private final String data;
+
+    public Message(MessageType type) {
+        this.type = type;
+        this.data = null;
+    }
+
+    public Message(MessageType type, String data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public String getData() {
+        return data;
+    }
 }

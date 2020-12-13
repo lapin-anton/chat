b214e5560f2fe9c04c24d2259db176b569a0aa05
lapin-anton <lapin_antoine@mail.ru>
2020-12-13 18:34:54 +0300

    added fields, constructor, empty methods "send" and "receive", method "close" into Connection class

--- app/src/Connection.java
+++ app/src/Connection.java
@@ -1,2 +1,33 @@
-public class Connection {
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Connection implements Closeable {
+
+    private final Socket socket;
+    private final ObjectInputStream in;
+    private final ObjectOutputStream out;
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        this.in = new ObjectInputStream(socket.getInputStream());
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+    }
+
+    public void send(Message message) {
+
+    }
+
+    public Message receive() {
+        return null;
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.socket.close();
+        this.out.close();
+        this.in.close();
+    }
 }

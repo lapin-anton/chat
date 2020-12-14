e782d3cc68df8c24e5f578944e763896ec87394e
lapin-anton <lapin_antoine@mail.ru>
2020-12-14 19:44:50 +0300

    added realization into method "run", empty realization of methods "serverHandshake", "notifyUsers", "serverMainLoop" of inner class Handler by class Server. Added field connectionMap into class Server.

--- app/src/Server.java
+++ app/src/Server.java
@@ -1,8 +1,13 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     public static void main(String[] args) {
         int port = ConsoleHelper.readInt();
         try (ServerSocket serverSocket = new ServerSocket(port)) {
@@ -26,7 +31,41 @@ public class Server {
 
         @Override
         public void run() {
+            String clientName = null;
+            ConsoleHelper.writeMessage(String.format("Установлено соединение с %s",
+                    socket.getRemoteSocketAddress().toString()));
+            try (Connection connection = new Connection(socket)){
+                clientName = serverHandshake(connection);
+                sendBroadcastMessage(new Message(MessageType.USER_ADDED, clientName));
+                notifyUsers(connection, clientName);
+                serverMainLoop(connection, clientName);
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage(String.format("Ошибка обмена данными с удаленным адресом %s",
+                        socket.getRemoteSocketAddress()));
+            } finally {
+                if(clientName != null) {
+                    connectionMap.remove(clientName);
+                    sendBroadcastMessage(new Message(MessageType.USER_REMOVED, clientName));
+                }
+                ConsoleHelper.writeMessage(String.format("Соединение с удаленным адресом %s закрыто",
+                        socket.getRemoteSocketAddress().toString()));
+            }
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            return null;
+        }
+
+        private void notifyUsers(Connection connection, String userName) throws IOException {
 
         }
+
+        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+
     }
 }

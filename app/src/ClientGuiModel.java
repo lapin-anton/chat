d69fc6da2513c3d0c75668a01870398d0908062f
lapin-anton <lapin_antoine@mail.ru>
2020-12-15 13:04:34 +0300

    added realization of ClientGuiModel class

--- app/src/ClientGuiModel.java
+++ app/src/ClientGuiModel.java
@@ -1,9 +1,28 @@
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ClientGuiModel {
+    private final Set<String> allUserNames = new HashSet<>();
+    private String newMessage;
+
     public String getNewMessage() {
-        return null;
+        return newMessage;
     }
 
-    public String[] getAllUserNames() {
-        return null;
+    public void setNewMessage(String newMessage) {
+        this.newMessage = newMessage;
+    }
+
+    public Set<String> getAllUserNames() {
+        return Collections.unmodifiableSet(allUserNames);
+    }
+
+    public void addUser(String newUserName) {
+        allUserNames.add(newUserName);
+    }
+
+    public void deleteUser(String userName) {
+        allUserNames.remove(userName);
     }
 }

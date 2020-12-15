355b32932b8a00d50e60ba4df819f70094e65b6a
lapin-anton <lapin_antoine@mail.ru>
2020-12-15 09:15:39 +0300

    added inner class SocketThread with empty realization into class Client

--- app/src/Client.java
+++ app/src/Client.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
 
     private Connection connection;
@@ -10,6 +13,42 @@ public class Client {
     }
 
     public void run() {
+        SocketThread socketThread = new SocketThread();
+    }
 
+    public class SocketThread extends Thread {
+
+        @Override
+        public void run() {
+            String host = getServerAddress();
+            int port = getServerPort();
+            try(Socket socket = new Socket(host, port)) {
+                connection = new Connection(socket);
+                clientHandshake();
+                clientMainLoop();
+            } catch (IOException | ClassNotFoundException e) {
+                notifyConnectionStatusChanged(false);
+            }
+        }
+
+        private void clientHandshake() throws IOException, ClassNotFoundException {
+
+        }
+
+        private void clientMainLoop() throws IOException, ClassNotFoundException {
+
+        }
+
+        private void notifyConnectionStatusChanged(boolean clientConnected) {
+
+        }
+    }
+
+    private String getServerAddress() {
+        return null;
+    }
+
+    private int getServerPort() {
+        return 0;
     }
 }

fc7e31c1941c0782317d02b2904c0add542af341
lapin-anton <lapin_antoine@mail.ru>
2020-12-15 09:23:43 +0300

    added empty methods "processIncomingMessage", "informAboutAddingNewUser", "informAboutDeletingNewUser" into inner class SocketThread

--- app/src/Client.java
+++ app/src/Client.java
@@ -32,14 +32,54 @@ public class Client {
         }
 
         private void clientHandshake() throws IOException, ClassNotFoundException {
-
+            while (true) {
+                Message message = connection.receive();
+                if(message.getType() == null) throw new IOException("Unexpected MessageType");
+                if(message.getType().equals(MessageType.NAME_REQUEST)) {
+                    connection.send(new Message(MessageType.USER_NAME, getUserName()));
+                } else if (message.getType().equals(MessageType.NAME_ACCEPTED)) {
+                    notifyConnectionStatusChanged(true);
+                    break;
+                } else {
+                    throw new IOException("Unexpected MessageType");
+                }
+            }
         }
 
         private void clientMainLoop() throws IOException, ClassNotFoundException {
-
+            while(true) {
+                Message message = connection.receive();
+                if(message.getType() == null) {
+                    throw new IOException("Unexpected MessageType");
+                }
+                if(message.getType().equals(MessageType.TEXT)) {
+                    processIncomingMessage(message.getData());
+                } else if (message.getType().equals(MessageType.USER_ADDED)) {
+                    informAboutAddingNewUser(message.getData());
+                } else if (message.getType().equals(MessageType.USER_REMOVED)) {
+                    informAboutDeletingNewUser(message.getData());
+                } else {
+                    throw new IOException("Unexpected MessageType");
+                }
+            }
         }
 
         private void notifyConnectionStatusChanged(boolean clientConnected) {
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this) {
+                Client.this.notify();
+            }
+        }
+
+        private void processIncomingMessage(String message) {
+
+        }
+
+        private void informAboutAddingNewUser(String userName) {
+
+        }
+
+        private void informAboutDeletingNewUser(String userName) {
 
         }
     }
@@ -50,5 +90,9 @@ public class Client {
 
     private int getServerPort() {
         return 0;
+    }
+
+    private String getUserName() {
+        return null;
     }
 }
